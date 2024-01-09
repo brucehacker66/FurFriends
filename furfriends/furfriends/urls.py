8af75175c0797670d29415926bf4b633782b1ef8
BruceZhang666 <yzhan475@jhu.edu>
2024-01-09 11:56:17 +0800

    page

--- furfriends/furfriends/urls.py
+++ furfriends/furfriends/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls')),
-]
+    path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+
+    re_path(r'^(?:.*)/?$',TemplateView.as_view(template_name="index.html"))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
